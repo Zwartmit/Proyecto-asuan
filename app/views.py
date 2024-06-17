88b15a6289019b7ee34d91001a3a7487aa637919
Zwartmit <brandonurbanobuz@gmail.com>
2024-06-17 12:17:37 -0500

    Se implementó Bootstrap y se añadieron los templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from app.models import *
 
-# Create your views here.
+def vista1(request):
+    nombre = {
+        'nombre': 'Juan'
+    }
+    return JsonResponse(nombre)
+
+def vista2(request):
+    nombre = {
+        'nombre': 'Juan',
+        'categorias': Categoria.objects.all()
+    }
+    return render(request, 'index.html', nombre)
