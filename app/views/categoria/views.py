104eb2abad6e09624b0f67aa16bcac485daf5294
Zwartmit <brandonurbanobuz@gmail.com>
2024-06-21 12:24:04 -0500

    Se implementó SweetAlert2, se hicieron validaciones para evitar datos repetidos, se avanzó en el crud

--- app/views/categoria/views.py
+++ app/views/categoria/views.py
@@ -1,12 +1,12 @@
 import django
 # from django.contrib.auth.decorators import login_required
 import os
+from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.http import JsonResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
 
 from app.models import Categoria
 from app.forms import CategoriaForm
@@ -17,6 +17,8 @@ def lista_categoria(request):
         'categorias': Categoria.objects.all()
     }
     return render(request, 'categoria/listar.html',nombre)
+
+###### LISTAR ######
 
 class CategoriaListView(ListView):
     model = Categoria
@@ -34,7 +36,12 @@ class CategoriaListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = 'Listado de Categorias'
+        context['entidad'] = 'Categorías'
+        context['listar_url'] = reverse_lazy('app:categoria_crear')
+        context['crear_url'] = reverse_lazy('app:categoria_crear')
         return context
+
+###### CREAR ######
 
 class CategoriaCreateView(CreateView):
     model = Categoria
@@ -45,4 +52,57 @@ class CategoriaCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = 'Crear categoria'
+        context['clase'] = 'Esta categoría ya existe'
+        context['entidad'] = 'Crear categoria'
+        context['listar_url'] = reverse_lazy('app:categoria_lista')
+        return context
+    
+    def form_valid(self, form):
+        categoria = form.cleaned_data.get('categoria').lower()
+        
+        if Categoria.objects.filter(categoria__iexact=categoria).exists():
+            form.add_error('categoria', 'Ya existe una categoría con este nombre.')
+            return self.form_invalid(form)
+        
+        return super().form_valid(form)
+    
+###### EDITAR ######
+
+class CategoriaUpdateView(UpdateView):
+    model = Categoria
+    form_class = CategoriaForm
+    template_name = 'categoria/crear.html'
+    success_url = reverse_lazy('app:categoria_lista')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Editar categoria'
+        context['clase'] = 'Esta categoría ya existe'
+        context['entidad'] = 'Categorías'
+        context['listar_url'] = reverse_lazy('app:categoria_lista')
+        return context
+    
+    def form_valid(self, form):
+        categoria = form.cleaned_data.get('categoria').lower()
+        
+        if Categoria.objects.filter(categoria__iexact=categoria).exists():
+            form.add_error('categoria', 'Ya existe una categoría con este nombre.')
+            return self.form_invalid(form)
+        
+        return super().form_valid(form)
+    
+###### ELIMINAR ######
+
+class CategoriaDeleteView(DeleteView):
+    model = Categoria
+    form_class = CategoriaForm
+    template_name = 'categoria/eliminar.html'
+    success_url = reverse_lazy('app:categoria_lista')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Eliminar categoria'
+        context['clase'] = 'Esta categoría ya existe'
+        context['entidad'] = 'Categorías'
+        context['listar_url'] = reverse_lazy('app:categoria_lista')
         return context
